72699a0f225ae88ece13cb041f751589920d0276
temkinman <temkinman@mail.ru>
2020-09-03 19:44:33 +0300

    add day03

---
+++ day_03/myscript.js
@@ -0,0 +1,28 @@
+alert("Я JavaScript!");
+
+let name = "Джон";
+let admin = name;
+alert(name);
+
+let earth;
+let currentUser;
+
+
+/*
+"" + 1 + 0   //10
+"" - 1 + 0 //-1
+true + false //1
+6 / "3" //2
+"2" * "3" // 6
+4 + 5 + "px" // 9px
+"$" + 4 + 5 //$45
+"4" - 2 //2
+"4px" - 2 // Nan
+7 / 0 //infinity
+"  -9  " + 5 //4
+"  -9  " - 5  //-14
+null + 1 //1
+undefined + 1 // NaN
+" \t \n" - 2 //
+    -2
+*/
