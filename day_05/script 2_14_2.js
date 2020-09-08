e075eb8d130fcb272a26fff63ea8abc66b615f9f
temkinman <temkinman@mail.ru>
2020-09-08 15:28:25 +0300

    add Day_05

---
+++ day_05/script 2_14_2.js
@@ -0,0 +1,13 @@
+function checkAge(age) {
+  return age > 18 ? true : confirm('Родители разрешили?');
+}
+
+function checkAge2(age) {
+  return age > 18 || confirm('Родители разрешили?');
+}
+
+console.log(checkAge(19));
+console.log(checkAge2(19));
+
+console.log(checkAge(18));
+console.log(checkAge2(18));
---
+++ day_05/script 2_14_4.js
@@ -0,0 +1,19 @@
+function pow(x, n) {
+  let result = x;
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+console.log(pow(3, 2));
+console.log(pow(3, 3));
+console.log(pow(1, 100));
+
+function powRefactor(x, n) {
+  return x**n;
+}
+
+console.log(powRefactor(3, 2));
+console.log(powRefactor(3, 3));
+console.log(powRefactor(1, 100));
