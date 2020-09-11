8cf09d7cac86380fe4e83acfcac30f4e4819dc0c
temkinman <temkinman@mail.ru>
2020-09-11 13:48:18 +0300

    refactor function readNumber

--- day_07/script_5_2.js
+++ day_07/script_5_2.js
@@ -1,7 +1,6 @@
 function readNumber() {
-  let number;
   do {
-    number = prompt("Enter number", '');
+    let number = prompt("Enter number", '');
     console.log('num = ' + number);
   }
   while (number != null && number != "" && !(+number))
